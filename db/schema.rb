86b1c3eee0a32ebcb25070bb27310f0f3e29012b
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-10-31 16:08:25 +0100

    Created Chat/Messages/Message/Message_form components

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_104944) do
+ActiveRecord::Schema.define(version: 2018_10_31_142805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chats", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "author"
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
